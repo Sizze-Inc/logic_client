a29005a037573e1da0ebcfd797114bd5c69f37b9
kabiljanz <kabiljanz0301@gmail.com>
2022-12-21 19:48:04 +0600

    add filters

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="sizze_logic_client",
     packages=find_packages(),
-    version='0.0.8',
-    description='fixed',
+    version='0.0.9',
+    description='add filters',
     author='kabu1zhan',
     license='Sizze',
 )
--- sizze_logic_client/client.py
+++ sizze_logic_client/client.py
@@ -1,10 +1,12 @@
-from sizze_logic_client.api import variables, actions
+from sizze_logic_client.api import variables, actions, filters
 
 
 variables_client = variables.VariableClient()
 actions_client = actions.ActionClient()
+filter_client = filters.FilterClient()
 
 
 def client(base_url: str):
     variables_client.set_base_url(base_url)
     actions_client.set_base_url(base_url)
+    filter_client.set_base_url(base_url)
